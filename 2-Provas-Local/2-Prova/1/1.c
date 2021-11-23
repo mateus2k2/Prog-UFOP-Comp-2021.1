e626df54117a929862867c2d542ed482e687c6e8
Mateus Filipe <mateusfilipe2k2@gmail.com>
2021-11-23 09:36:37 -0300

    link

--- 2-Provas-Local/2-Prova/1/1.c
+++ 2-Provas-Local/2-Prova/1/1.c
@@ -18,3 +18,5 @@ int main(int argc, char *argv[ ]){
 
     return 0;
 }
+
+// ln -s /mnt/c/Users/mateu/Downloads/FACULDADE/2-COMP/1-Periodo/PROGRAMACAO/Praticas/Provas-Local/2-Prova/1  /home/mateus/USER/Praticas\ -\ Prog\ -\ Comp/2-Provas-Local
