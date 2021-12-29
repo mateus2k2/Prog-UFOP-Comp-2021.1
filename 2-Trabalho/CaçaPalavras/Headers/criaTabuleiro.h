48cd9a50de088041e76d66cfb5beb9a32e78974a
Mateus <mateusfilipe2k2@gmail.com>
2021-12-28 23:24:41 -0300

    Delete 2-Trabalho directory

--- 2-Trabalho/CaçaPalavras/Headers/criaTabuleiro.h
+++
@@ -1,6 +0,0 @@
-//Nome: Mateus Filipe Moreira Silva
-//Matrícula: 2114156
-//Turma  41
-
-void criarJogo();
-void colocaPalavra(int escolhaDirecao, int *contColocadas, int lim, int col, int tamLin, int tamCol, Palavra *palavras ,Item **tabuleiro, int *contDirecao);
